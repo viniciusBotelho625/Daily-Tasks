e395efa075abad5a28302ab9371ddd6c04cbdff9
viniciusBotelho625 <58275652+viniciusBotelho625@users.noreply.github.com>
2020-10-27 00:18:20 -0300

    Tratando os erros + dependencias

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,15 +1,25 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 const routesUsers = require('./routes/usuarios');
 
+app.use(morgan('dev'));
 app.use('/users', routesUsers);
 
 app.use((req, res, next) => {
-    res.status(200).send({
-        mensagem: 'OK, Deu certo!'
+    const erro = new Error('Não encontrado!');
+    erro.status = 404;
+    next(erro);
+}); 
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    return res.send({
+        erro: {
+            mensagem: error.message
+        }
     });
 });
-
 
 module.exports = app;
