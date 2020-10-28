418e9b4331934947c91c329f769827b196c13c90
viniciusBotelho625 <58275652+viniciusBotelho625@users.noreply.github.com>
2020-10-27 23:31:13 -0300

    Metodo listar usuários

--- backend/src/routes/usuarios.js
+++ backend/src/routes/usuarios.js
@@ -3,12 +3,23 @@ const router = express.Router();
 const mysql = require('../mysql').pool;
 
 router.get('/', (req, res, next) => {
-    res.status(200).send({
-        mensagem: 'Usando o GET dentro da rota de usuarios'
-    });
+    mysql.getConnection((error, conn) => {
+        if (error) { return res.status(500).send({ error: error })}
+        conn.query(
+            'SELECT * FROM usuarios',
+            (error, resultado, field) => {
+                if (error) { return res.status(500).send({ error: error })}
+                return res.status(200).send({response: resultado})
+            }
+        )
+    })
 });
 
-router.post('/', (req, res, next) => {
+// res.status(200).send({
+    //     mensagem: 'Usando o GET dentro da rota de usuarios'
+    // });
+
+router.post('/create', (req, res, next) => {
 
     const usuario = {
         name: req.body.name,
@@ -17,17 +28,13 @@ router.post('/', (req, res, next) => {
     };
 
     mysql.getConnection((error, conn) => {
+        if (error) { return res.status(500).send({ error: error })}
         conn.query(
             'INSERT INTO usuarios (nome, email, senha) VALUES (?, ?, ?)',
             [req.body.name, req.body.email, req.body.password],
             (error, resultado, field) => {
                 conn.release();
-                if (error) {
-                    return res.status(500).send({
-                        error: error,
-                        response: null
-                    });
-                }
+                if (error) { return res.status(500).send({ error: error })}
 
                 res.status(201).send({
                     mensagem: 'Usuário criado com sucesso!',
@@ -38,18 +45,19 @@ router.post('/', (req, res, next) => {
     });
 });
 
-router.delete('/:id_user', (req, res, next) => {
-    const id = req.params.id_user
-    res.status(200).send({
-        mensagem: 'Usando o DELETE com um usuário exclusivo',
-        id: id
-    });
-});
 
 router.put('/:id_user', (req, res, next) => {
     const id = req.params.id_user
     res.status(200).send({
         mensagem: 'Usando o PUT com um usuário exclusivo',
+        id: id
+    });
+});
+
+router.delete('/:id_user', (req, res, next) => {
+    const id = req.params.id_user
+    res.status(200).send({
+        mensagem: 'Usando o DELETE com um usuário exclusivo',
         id: id
     });
 });
