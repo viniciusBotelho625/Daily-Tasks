93b2585e3fb6f1d63d2bcd96738334cc121df17b
viniciusBotelho625 <58275652+viniciusBotelho625@users.noreply.github.com>
2020-10-30 20:08:50 -0300

    Criar, deletar e alterar status do projeto

--- backend/src/routes/tasks.js
+++ backend/src/routes/tasks.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const mysql = require('../mysql').pool;
 
 
+//Listar todos lembretes não concluidos
 router.get('/:id_user', (req, res, next) => {
     mysql.getConnection((error, conn) =>{
         if (error) { return res.status(500).send({ error: error })}
@@ -19,5 +20,96 @@ router.get('/:id_user', (req, res, next) => {
         )
     })
 });
+
+//Listar todos lembretes concluidos
+router.get('/concluidos/:id_user', (req, res, next) => {
+    mysql.getConnection((error, conn) =>{
+        if (error) { return res.status(500).send({ error: error })}
+        conn.query(
+            `SELECT L.id, L.dt_cadastro, L.dt_termino, L.titulo, L.descricao, L.concluido, U.nome 
+            FROM lembretes AS L
+            JOIN usuarios AS U ON U.id = L.usuario_id
+            WHERE L.concluido = true AND U.id = ?`,
+            [req.params.id_user],
+            (error, resultado, field) => {
+                if (error) { return res.status(500).send({ error: error })}
+                return res.status(200).send({response: resultado})
+            }
+        )
+    })
+});
+
+router.post('/create', (req, res, next) => {
+
+    const tasks = {
+        date_start: req.body.date_start,
+        date_end: req.body.date_end,
+        titlle: req.body.title,
+        description: req.body.description,
+        concluded: false,
+        user_id: req.body.user_id
+    };
+
+    mysql.getConnection((error, conn) => {
+        if (error) { return res.status(500).send({ error: error })}
+        conn.query(
+            `INSERT INTO lembretes (dt_cadastro, dt_termino, titulo, descricao, concluido, usuario_id) 
+            VALUES (?, ?, ?, ?, FALSE, ?)`,
+            [
+                req.body.date_start,
+                req.body.date_end,
+                req.body.title,
+                req.body.description,
+                req.body.user_id
+            ],
+            (error, resultado, field) => {
+                conn.release();
+                if (error) { return res.status(500).send({ error: error })}
+
+                res.status(201).send({
+                    mensagem: 'Lembrete criado com sucesso!',
+                    id_lembrete: resultado.insertId
+                });
+            }
+        ) 
+    })
+});
+
+router.delete('/', (req, res, next) => {
+    mysql.getConnection((error, conn) => {
+        if (error) { return res.status(500).send({ error: error })}
+        conn.query(
+            'DELETE FROM lembretes WHERE id = ?',
+            [req.body.id],
+            (error, resultado, field) => {
+                if (error) { return res.status(202).send({ error: error })}
+
+                res.status(202).send({
+                    mensagem: 'Usuário removido com sucesso!',
+                });
+            }
+        )
+    })
+});
+
+//Alterar status do projeto => (Fazer ainda o jeito de descobrir qual ID ta sendo passado)
+router.put('/', (req, res, next) => {
+    mysql.getConnection((error, conn) =>{
+        if (error) { return res.status(500).send({ error: error })}
+        conn.query(
+            'UPDATE lembretes SET concluido = true WHERE id = ?',
+            [req.body.id],
+            (error, resultado, field) => {
+                if (error) { return res.status(202).send({ error: error })}
+
+                res.status(202).send({
+                    mensagem: 'Lembrete alterado com sucesso!',
+                });
+            }
+        )
+    })
+});
+
+
 
 module.exports = router;
